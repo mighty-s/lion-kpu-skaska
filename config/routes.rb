00c67d5968ee93d56e5a15f7e95a33a51e89fa67
OctopusSwellfish <010-2648-8131@daum.net>
2018-08-19 01:27:12 +0900

    post방식으로 변경 ...했어요

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   get 'notify/create'
 
+  post 'notify/index'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
