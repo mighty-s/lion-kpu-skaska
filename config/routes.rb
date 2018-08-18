036a06143d00a8d7ad7b751c93cc8a538b6d8bfa
OctopusSwellfish <010-2648-8131@daum.net>
2018-08-19 00:49:52 +0900

    공지사항 작성 프론트 만들었어요 <!-- 작성하기버튼에 까만색 옷 입히기 해야됨 외않되? -->

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get 'notify/show'
 
+  get 'notify/create'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
