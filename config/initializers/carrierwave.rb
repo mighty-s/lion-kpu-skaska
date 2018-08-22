5489f196d7187eb8d03d167df052cc38f0c17384
sight-indomitable <sdm32852naver.com>
2018-08-22 18:33:45 +0900

    AWS-S3파일 업로드 및 보안이슈 확인

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,16 @@
 require 'carrierwave/orm/activerecord'
 
-
+CarrierWave.configure do |config|
+  # AWS S3 configs
+  # ENV -> 리눅스 환경변수, 설동민 pc에 있음
+  #
+  config.fog_provider    = 'fog/aws' # required
+  config.fog_credentials = {
+    provider:              'AWS', # required
+    aws_access_key_id:     ENV['AWS_KEY'],        # required unless using use_iam_profile
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'], # required unless using use_iam_profile
+    use_iam_profile:       true,                       # optional, defaults to false
+    region:                'ap-northeast-2',           # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory = 'kpu-likelion-skaska' # required
+end
