a0f39da6ac8d24f4c383e3bde2b04c1e7958170b
OctopusSwellfish <010-2648-8131@daum.net>
2018-08-24 14:49:46 +0900

    데이터베이스 수정, 데이터베이스 추가했습니다! 추가한 데이터베이스에는 아직 설정 안해써용

--- db/migrate/20180821133059_create_comment_recipes.rb
+++ db/migrate/20180821133059_create_comment_recipes.rb
@@ -7,7 +7,9 @@
 class CreateCommentRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :comment_recipes do |t|
-
+      t.belongs_to :user        #외래키, 댓글 작성자의 ID
+      t.belongs_to :recipe      #외래키, 레시피의 ID
+      t.text :content
       t.timestamps
     end
   end
