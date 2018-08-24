1e972940de2813710780665e8a9ebfeaf8b13218
OctopusSwellfish <010-2648-8131@daum.net>
2018-08-24 14:54:54 +0900

    데이터베이스 전면수정했습니다

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ApplicationRecord
+  belongs_to :user  #유저와의 1:N관계
+  belongs_to :recipe #레시피와의 1:N관계
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   # 외래키 관계
   belongs_to :user
   has_many :recipe_images
+  has_many :likes
 
 
   # create 메서드가 너무 길어져서 이걸로 뺏음
