11e213e041276e90d81983e3bdb0c00371e0ba05
OctopusSwellfish <010-2648-8131@daum.net>
2018-08-24 16:45:02 +0900

    이것저것

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     # Inner Join을 통해서 유저의 별명, 첫번째 첨부 사진까지 같이 가져온다.
     #
     # 3중 inner join --> (Recipe, User, RecipeImage)
-    @r_list = Recipe.select('recipes.*, recipe_images.id , users.nickname').joins(:recipe_images, :user)
+    @r_list = Recipe.select('recipes.*, recipe_images.id , users.nickname').joins(:recipe_images, :user).group("recipes.id")
   end
 
   def search
